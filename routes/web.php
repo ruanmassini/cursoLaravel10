9b6d03da609e25c2b2117812799e394a78e9c4a0
Ruan Pablo Massini <ruanmassini@gmail.com>
2024-01-03 11:26:41 -0400

    last pull

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/', function () {
     return view('index');
 });
 
+// Produtos:
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProductsController::class, 'index'])->name('productIndex');
+});
