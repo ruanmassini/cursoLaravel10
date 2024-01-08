d4f6e119729edf62c2fc3095649a4f381f24fac2
Ruan Pablo Massini <ruanmassini@gmail.com>
2024-01-08 11:41:57 -0400

    added pagination for the class products (after this i will do the CRUD)

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,14 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function index()
     {
         $findProduto = Product::all();
-        dd($findProduto);
-        return 'produtos';
+        return view('pages.produtos.paginacao', compact('findProduto'));
     }
 }
